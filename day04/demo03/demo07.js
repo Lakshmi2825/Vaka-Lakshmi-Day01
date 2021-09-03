d71fd74c7e1d3e409830c58618be18075e81f083
Lakshmi2825 <89259569+Lakshmi2825@users.noreply.github.com>
2021-09-03 12:45:11 +0530

    Delete demo07.js

--- day04/demo03/demo07.js
+++
@@ -1,8 +0,0 @@
-function doSomething(msg, funref) {
-    console.log("Dosomething called" + msg);
-    funref();
-}
-
-doSomething("using arrow function", () => {
-    console.log("using => arrow function we are working");
-})
