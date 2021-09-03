f8435674ec878cbefc23860dcfd2824a46c9293f
Lakshmi2825 <89259569+Lakshmi2825@users.noreply.github.com>
2021-09-03 12:44:17 +0530

    Delete demo02.js

--- day04/demo03/demo02.js
+++
@@ -1,9 +0,0 @@
-//anonymous -> no name 
-//In javascript everything must be name 
-//hence anonymous functon must be having some 
-//reference varialbe
-console.log("Anonymous function demo")
-const myFunction = function() {
-    console.log("Anonymous function");
-}
-myFunction();
