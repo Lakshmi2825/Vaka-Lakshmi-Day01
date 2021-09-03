325594ca1d7256dbf3ce70b10bf549afca2c75b9
Lakshmi2825 <89259569+Lakshmi2825@users.noreply.github.com>
2021-09-03 12:45:00 +0530

    Delete demo06.js

--- day04/demo03/demo06.js
+++
@@ -1,8 +0,0 @@
-function doSomething(msg, funRef) {
-    console.log(msg);
-    funRef();
-}
-
-doSomething("Providing anonymous function", function() {
-    console.log("Called only once with dosomething")
-});
